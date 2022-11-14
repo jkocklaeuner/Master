cf7f705532c294fd60bea7b70d8a5feff19ce333
jkocklaeuner <jkocklaeuner@cpch02.chm.tu-dresden.de>
2022-11-14 12:38:15 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+setuptools.setup(
+
+    name="nnqs-master", # Replace with your username
+
+    version="1.0.0",
+
+    author="jkocklaeuner",
+
+    description="NNQS for Quantum Chemistry: Master project",
+
+    url="<https://github.com/jkocklaeuner/Master.git>",
+
+    packages=setuptools.find_packages(),
+    
+    install_requires=[
+        "netket==3.4.2",
+        "openfermion",
+        "matplotlib",
+        "jaxlib>=0.1, <0.3"	]
+        
+)
