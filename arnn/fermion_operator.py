9bbfea49cd74507c7e76b0aa653ef997973ced9a
Jannis Kockläuner <106096972+jkocklaeuner@users.noreply.github.com>
2022-11-14 14:05:54 +0100

    Update fermion_operator.py

--- arnn/fermion_operator.py
+++ arnn/fermion_operator.py
@@ -1,17 +1,3 @@
-# Copyright 2022 The NetKet Authors - All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from typing import List, Union, Optional
 import re
 from collections import defaultdict
@@ -295,7 +281,7 @@ class ElectronicOperator(AbstractOperator):
 
         if self.diag:
             val = eigsh(self.h,k=1)
-            self.eig_vals = np.append(self.eig_vals[0],val) 
+            self.eig_vals = np.append(self.eig_vals,val[0]) 
         end = time.time()
         self.time += end - start
         #print(f"Energy calculation took {end - start} seconds")
